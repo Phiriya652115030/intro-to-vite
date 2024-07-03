44d92fc308576b481c7eb55abf435612b73ab2e0
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-03 13:00:35 +0700

    9.3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,6 +43,8 @@
 </script>
 
 <template>
+  <h1>Events For Good</h1>
+    <!-- new element-->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <EventDetails v-for="event in events" :key="'details-' + event.id" :event="event" />
