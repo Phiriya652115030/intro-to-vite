b1696672400777471f75d0e11a30e2b04a15a9e2
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-03 10:28:26 +0700

    update 5.0

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -13,12 +13,12 @@
   padding: 20px;
   width: 250px;
   cursor: pointer;
-  border: 1px solid #39496c;
+  border: 1px solid #39495c;
   margin-bottom: 18px;
 }
 
 .event-card:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.21);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
