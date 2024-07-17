0b0447432481a8b7e8b27269ee487a418fd5e40f
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-17 23:03:06 +0700

    1.5

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'https://localhost:3004/events/123',
+    baseURL: 'https://localhost:3000/events/123',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
