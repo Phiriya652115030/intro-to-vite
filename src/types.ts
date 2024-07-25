1fac52873bee22a052670a69d651894d38583112
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-26 00:26:26 +0700

    L4 1.2

--- src/types.ts
+++ src/types.ts
@@ -8,12 +8,16 @@ export interface Event {
     time: string
     petsAllowed: boolean
     organizer: string
-}
-export interface Student {
+  }
+  export interface Student {
     id: number
     studentId: string
     name: string
+    surname: string
     gpa: number
     image: string
     description: string
+  }
+  export interface MessageState {
+    message: string
   }
