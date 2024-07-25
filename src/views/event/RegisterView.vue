d014df7af11d6fba320558e0ebe295014161ed93
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-26 00:28:22 +0700

    1.4

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
+import type { Event } from '@/type'
 import { toRefs } from 'vue'
-import { type Event } from '@/types'
 import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
-    event: Event
-    id: String
+  event: Event
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
 const router = useRouter()
+const store = useMessageStore()
 const register = () => {
-    //If the registration API call successful
-    //Push back to the event detail view
-    router.push({ name: 'event-detail-view' })
+  //
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
 }
 </script>
 <template>
