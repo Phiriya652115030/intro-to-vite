42c3e5db8ed0f64f9284ff64b94f17a933032b75
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-25 16:49:05 +0700

    2.9

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -41,20 +41,24 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <EventDetails v-for="event in events" :key="'details-' + event.id" :event="event" />
-  </div>
+  <div class="pagination">
   <RouterLink
+    id="page-prev"
     :to="{ name: 'event-list-view', query: { page: page - 1 } }"
     rel="prev"
     v-if="page != 1"
-    >Prev Page</RouterLink
+    >&#60; Prev Page</RouterLink
     >
 
     <RouterLink 
+    id="page-next"
     :to="{ name: 'event-list-view',query: { page: page + 1 } }"
     rel="next"
     v-if="hasNextPage"
-    >Next Page</RouterLink
+    >Next Page &#62;</RouterLink
     >
+  </div>
+</div>
 </template>
 
 <style scoped>
@@ -62,5 +66,20 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+  .pagination {
+    display: flex;
+    width: 290px;
+  }
+  .pagination a {
+    flex: 1;
+    text-decoration: none;
+    color: #2c3e50;
+  }
+  #page-prev {
+    text-align: left;
+  }
+  #page-next {
+    text-align: right;
   }
 </style>
