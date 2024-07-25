fbd04bc5f2f9a264d0a3b3de35a1fd5c6b6e82cd
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-07-25 16:21:10 +0700

    1.8

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -17,7 +17,7 @@ const event = ref ({
 </script>
 
 <template>
-  <RouterLink to="{ name: 'event-detail-view', params: { id: event.id } }">
+  <RouterLink class="event-link" to="{ name: 'event-detail-view', params: { id: event.id } }">
   <div class="event-class">
     <div class="event-card">
       <h2>{{ props.event.title }}</h2>
@@ -39,5 +39,9 @@ const event = ref ({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
