27aeea8df2c6099d5f1e744a2cdef505cf460d25
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-08-08 15:22:30 +0700

    updates types build

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import EventCard from '@/components/EventCard.vue';
   import EventDetails from '@/components/EventDetails.vue'
-  import { type Event } from '@/types'
+  import type { Event } from '@/types'
   import { ref, onMounted, computed, watchEffect } from 'vue'
   import EventService from '@/services/EventService'
 
