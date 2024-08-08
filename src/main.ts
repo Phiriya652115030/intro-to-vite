3b3517470713dca0367c71bfe15e8e9d92dec75d
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-08-08 10:50:45 +0700

    L5-1.4

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-//import './assets/main.css'
+// import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import { createAppRouter } from './router'
+// nprogress
 import 'nprogress/nprogress.css'
+
 const app = createApp(App)
+const pageLimit = [null, 3, 1, 2, 1]
 
 app.use(createPinia())
-app.use(router)
+app.use(createAppRouter(pageLimit))
 
 app.mount('#app')
