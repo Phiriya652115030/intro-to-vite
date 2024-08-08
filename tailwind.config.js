fa9b6eeb8f8b95f92572f3546c146a47b49f2794
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-08-08 11:13:56 +0700

    L5 7.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,11 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)'
-      }
+        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
+      },
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [],
