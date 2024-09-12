060abf63339a3f651833a7853cfdabf1ba98da79
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-09-12 23:25:38 +0700

    7.10

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -20,9 +20,11 @@ const props = defineProps<{
 <template>
   <RouterLink class="text-[#2c3e50] no-underline" :to="{ name: 'event-detail-view', params: { id: event.id } }">
   <div class="event-class">
-    <div class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp">
+    <div 
+    class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp"
+    >
       <h2>{{ props.event.title }}</h2>
-      <span>@{{ props.event.time }} on {{ props.event.date }}</span>
+      <span>{{ event.category }} @ {{ event.location }}</span>
     </div>
   </div>
 </RouterLink>
