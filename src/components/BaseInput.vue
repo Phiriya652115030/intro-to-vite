a5f115f51a074d2e68ff09a0a28d000809d109ff
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-09-20 10:02:21 +0700

    5.1

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineModel } from 'vue';
+  
+interface BaseInputProps {
+    label: string;
+}
+  
+const props = withDefaults(defineProps<BaseInputProps>(), {
+    label: '',
+});
+  
+const modelValue = defineModel();
+</script>
+
+<template>
+    <label v-if="label">
+      {{ props.label }}
+    </label>
+    <input class="mb-6" v-bind="$attrs" v-model="modelValue" placeholder="label" />
+  </template>
