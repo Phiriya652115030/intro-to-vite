6915940f80447bcf9487a9f909cfeadd255f8f42
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-10-03 23:25:56 +0700

    2.2

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -18,5 +18,9 @@ export default {
     },
     saveEvent(event: Event) {
         return apiClient.post('/events', event)
-    }
+    },
+    getEventsByKeyword(keyword: string, perPage: number, page: number):
+        Promise<AxiosResponse<EventItem[]>> {
+            return apiClient.get<EventItem[]>('/events?title=' + keyword + '&_limit=' + perPage + '&_page=' + page)
+        }
 }
